a581c85f2f4c131ee75d8870e88c7028f199577b
Евгений Москалянов <emos@inbox.ru>
2024-01-06 16:01:18 +0300

    Рефакторинг сервиса инвестиций.

--- app/crud/__init__.py
+++ app/crud/__init__.py
@@ -1,2 +1,3 @@
+from .base import BaseCRUD  # noqa
 from .charity_project import CharityProjectCRUD  # noqa
 from .donation import DonationCRUD  # noqa
--- app/services/investment.py
+++ app/services/investment.py
@@ -3,69 +3,66 @@ from datetime import datetime
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app import models
-from app.crud import DonationCRUD, CharityProjectCRUD
+from app.crud import BaseCRUD
 
 
 async def require_investment(
     required_amount: int, session: AsyncSession
 ) -> int:
-    assert required_amount > 0
-    received_amount = 0
-    donations = await DonationCRUD().get_all_opened(session)
-    for donation in donations:
-        received_amount += get_money(
-            donation, required_amount - received_amount
-        )
-        session.add(donation)
-        if received_amount >= required_amount:
-            break
-
-    return received_amount
+    """Запрашивает требуемую сумму инвестиций из пожертвований.
+    Возвращает предоставленную пожертвованиями сумму инестиций.
+    """
+    return await _do_transfers(models.Donation, required_amount, session)
 
 
 async def distribute_investment(
     full_amount: int, session: AsyncSession
 ) -> int:
-    assert full_amount > 0
-    distributed_amount = 0
+    """Направляет имеющуюся сумму инвестиций в проекты.
+    Возвращает принятую проектами сумму инестиций.
+    """
+    return await _do_transfers(models.CharityProject, full_amount, session)
 
-    charity_projects = await CharityProjectCRUD().get_all_opened(session)
-    for charity_project in charity_projects:
-        distributed_amount += put_money(
-            charity_project, full_amount - distributed_amount
+
+async def _do_transfers(
+    model, full_amount: int, session: AsyncSession
+) -> int:
+    """Перемещает заданное количество средств из открытых пожертвований
+    или в открытые проекты (задаётся параметром model).
+    Возвращает реально перемещённое количество средств.
+    """
+    assert full_amount > 0
+    transferred_amount = 0
+    objs = await BaseCRUD(model).get_all_opened(session)
+    for obj in objs:
+        transferred_amount += _transfer_money(
+            obj, full_amount - transferred_amount
         )
-        session.add(charity_project)
-        if distributed_amount >= full_amount:
+        session.add(obj)
+        if transferred_amount >= full_amount:
             break
 
-    return distributed_amount
+    return transferred_amount
 
 
-def get_money(donation: models.Donation, required_amount: int):
+def _transfer_money(
+    obj: models.Donation | models.CharityProject,
+    required_amount: int
+) -> int:
+    """Забирает заданное количество средств из открытого пожертвования
+    или помещает заданное количество средств в открытый проект
+    (задаётся параметром obj).
+    Возвращает реально перемещённое количество средств.
+    """
     assert required_amount > 0
-    assert not donation.fully_invested
-    free_amount = donation.full_amount - donation.invested_amount
-    assert free_amount > 0
-    if required_amount >= free_amount:
-        donation.invested_amount = donation.full_amount
-        donation.fully_invested = True
-        donation.close_date = datetime.now()
-        return free_amount
+    assert not obj.fully_invested
+    rest_amount = obj.full_amount - obj.invested_amount
+    assert rest_amount > 0
+    if required_amount >= rest_amount:
+        obj.invested_amount = obj.full_amount
+        obj.fully_invested = True
+        obj.close_date = datetime.now()
+        return rest_amount
 
-    donation.invested_amount += required_amount
-    return required_amount
-
-
-def put_money(charity_project: models.CharityProject, required_amount: int):
-    assert required_amount > 0
-    assert not charity_project.fully_invested
-    free_amount = charity_project.full_amount - charity_project.invested_amount
-    assert free_amount > 0
-    if required_amount >= free_amount:
-        charity_project.invested_amount = charity_project.full_amount
-        charity_project.fully_invested = True
-        charity_project.close_date = datetime.now()
-        return free_amount
-
-    charity_project.invested_amount += required_amount
+    obj.invested_amount += required_amount
     return required_amount
