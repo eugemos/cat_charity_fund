ec6da1cf0d00a13fbfb4e332a574fffd418a5696
Евгений Москалянов <emos@inbox.ru>
2024-06-23 07:40:06 +0300

    Добавлен .env.template. Рефакторинг config.py

--- app/core/config.py
+++ app/core/config.py
@@ -5,12 +5,12 @@ from pydantic import BaseSettings, EmailStr
 
 class Settings(BaseSettings):
     app_author: str = 'Москалянов Евгений'
-    database_url: str = 'sqlite+aiosqlite:///./fastapi.db'
     app_title: str = 'API QRKot'
-    description: str = 'Финальный проект по FastAPI'
-    secret: str = 'SECRET'
+    description: str = 'Сервис пожертвований для благотворительного фонда'
+    secret: str = 'TOPSECRETs'
     first_superuser_email: Optional[EmailStr] = 'admin@admin.com'
     first_superuser_password: Optional[str] = 'ZZaaqq11'
+    database_url: str = 'sqlite+aiosqlite:///./fastapi.db'
 
     class Config:
         env_file = '.env'
